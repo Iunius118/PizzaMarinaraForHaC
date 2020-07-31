aba1d35198d5fb9158fb5443eeee52c820608c51
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2020-07-31 21:13:28 +0900

    Update to v1.12.2-1.1.0.1

--- src/main/java/com/github/iunius118/pizzamarinaraforhac/PizzaMarinaraForHaC.java
+++ src/main/java/com/github/iunius118/pizzamarinaraforhac/PizzaMarinaraForHaC.java
@@ -29,7 +29,7 @@ public class PizzaMarinaraForHaC
 {
     public static final String MOD_ID = "pizzamarinaraforhac";
     public static final String NAME = "Pizza Marinara for HaC";
-    public static final String VERSION = "1.12.2-1.1.0.0";
+    public static final String VERSION = "1.12.2-1.1.0.1";
     public static final String DEPENDENCIES = "required-after:dcs_lib@[3.3.1,);required:dcs_climate";
 
     @SidedProxy(
